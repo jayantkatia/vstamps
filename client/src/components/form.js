f624acb57132f251c1209b69d41ed285ae8225dc
Jayant Katia <jayantkatia65@gmail.com>
2022-08-24 16:39:54 +0530

    cosmetic: revamped

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,16 +1,27 @@
 import '../styles/Form.css'
 
+const Divider = () => {
+    return (
+        <div className="hr-with-text">
+            <hr />
+            OR
+            <hr />
+        </div>
+    );
+}
+
 const Form = () => {
     return (
         <div className="form-container">
             <form>
+                <input type="text" id="url" name="url" placeholder="Enter YouTube link" autocomplete="off"  required />
+                <Divider />
                 <div className="drag-container">
                     <p>Drag or Click on the button to upload video/audio files</p>
-                    <input type="file" accept="video/*,audio/*" id="user-video" name="file" required/>
+                    <input type="file" accept="video/*,audio/*" id="user-video" name="file" required />
                     <label className="button" htmlFor="user-video">Select video/audio</label>
                 </div>
-                <input type="text" id="user-keyword" name="keyword" placeholder="Enter Keyword" required/>
-                <input type="submit" className="button"/>
+                <input type="submit" className="button" />
             </form>
         </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/'  element={<App/>} />
+        <Route path='/result' element={<p>result</p>} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
